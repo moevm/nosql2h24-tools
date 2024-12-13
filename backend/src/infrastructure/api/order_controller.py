759a38b1e34c2b462cb274c2f0df8ffcc391c6e5
Анастасия Шиняева <nastya.shinyaeva1009@mail.ru>
2024-12-13 17:47:46 +0300

    role required fix

--- backend/src/infrastructure/api/order_controller.py
+++ backend/src/infrastructure/api/order_controller.py
@@ -3,6 +3,7 @@ from fastapi.security import OAuth2PasswordBearer
 
 from src.core.entities.order.order import OrderCreate, OrderCreated
 from src.core.services.order_service.order_service import OrderService
+from src.infrastructure.api.security.role_required import role_required
 from src.infrastructure.services_instances import get_order_service
 
 
@@ -14,6 +15,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
     path = "/",
     status_code=201,
     response_model=OrderCreated)
+@role_required('self')
 async def create_order(
     data: OrderCreate,
     order_service: OrderService = Depends(get_order_service),
