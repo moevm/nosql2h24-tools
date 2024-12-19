dcd01f74a933906f2fce65ff1fa1ce684473f57f
Анастасия Шиняева <nastya.shinyaeva1009@mail.ru>
2024-12-19 03:27:14 +0300

    export file added

--- backend/src/core/services/im_ex_service/im_ex_service.py
+++ backend/src/core/services/im_ex_service/im_ex_service.py
@@ -1,3 +1,5 @@
+import json
+
 from src.core.entities.db_model.db_model import DBModel, DBModelCreate
 from src.core.repositories.im_ex_repo.iim_ex_repo import IImExRepository
 
@@ -13,3 +15,10 @@ class ImExService:
 
     async def import_data(self, data: DBModelCreate) -> None:
         return await self.im_ex_repository.import_data(data)
+
+    async def export_file(self) -> str:
+        data = await self.im_ex_repository.export_data()
+        file_path = '/tmp/data.json'
+        with open(file_path, "w") as file:
+            json.dump(data.model_dump(), file, default=str)
+        return file_path
--- backend/src/infrastructure/api/import_export_controller.py
+++ backend/src/infrastructure/api/import_export_controller.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordBearer
+from fastapi.responses import FileResponse
 
 from src.core.entities.db_model.db_model import DBModel, DBModelCreate
 from src.core.services.im_ex_service.im_ex_service import ImExService
@@ -11,6 +12,18 @@ im_ex_router = APIRouter()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
+@im_ex_router.get(path="/export_file", status_code=201)
+async def export(
+        im_ex_service: ImExService = Depends(get_im_ex_service),
+        token: str = Depends(oauth2_scheme),
+):
+    is_worker(token)
+    file_path = await im_ex_service.export_file()
+    return FileResponse(
+        path=file_path,
+        filename="data.json",
+        media_type='application/octet-stream'
+    )
 
 @im_ex_router.get(path="/export", status_code=201, response_model=DBModel)
 async def export(
